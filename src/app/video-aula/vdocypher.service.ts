3a142daa82c6393bb57506a16fd30e15c2a98225
oliverbill <alves.bill@gmail.com>
2018-04-19 23:23:42 -0300

    correcao do cabecalho na chamada do getLivros do VdoCypherService

--- src/app/video-aula/vdocypher.service.ts
+++ src/app/video-aula/vdocypher.service.ts
@@ -4,13 +4,14 @@ import { of } from 'rxjs/observable/of';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 import { ErrorUtils } from '../error.utils';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class VdocypherService {
 
   readonly api_secret: String = '90064023e00c41c7b2c19041143da8f15c8c883eb1484dbbadacb154338ed47d';
   readonly videoPlayer = '#videoplayer';
-  readonly URL_GET_VIDEOS = 'https://dev.vdocipher.com/api/videos';
+  readonly URL_GET_VIDEOS = 'https://dev.vdocipher.com/api/videos';  
 
   private otp_code: Object;
 
@@ -25,36 +26,35 @@ export class VdocypherService {
   getOTP(id):Observable<any>{
     const url = `${this.URL_GET_VIDEOS}/${id}/otp`;
     
-    return this.client.get(url,{headers: new HttpHeaders(`Authorization: Apisecret ${this.api_secret}`)})
+    return this.client.get(url,{headers: new HttpHeaders({ [`Authorization:`]:"Apisecret ${this.api_secret}"})})
         .pipe(
             tap(o => console.log(`OTP: ${JSON.stringify(o)}`)),
             catchError(ErrorUtils.handleError<any>('get_OTP_code')));
   }
 
-  getVideo(id) {    
+  getVideo(id?):Observable<any> {    
    // nao funciona atribuir variavel externa dentro do lambda
-    return this.getOTP(id)
-     .pipe(
-        tap(
-            o => { return new VdoPlayer({
-                    otp: o,
-                    playbackInfo: btoa(JSON.stringify({
-                      videoId: id
-                    })),
-                    theme: '9ae8bbe8dd964ddc9bdb932cca1cb59a',        
-                    container: document.querySelector( this.videoPlayer ),
-                  });
-              }),
-              catchError(ErrorUtils.handleError<any>('getVideo'))
-      );    
+    if(id){
+      const url = `${this.URL_GET_VIDEOS}/${id}/`;
+      
+      this.client.get(url,{headers: new HttpHeaders(`authorization: Apisecret ${this.api_secret}`)})
+        .map(res=>res.json()).subscribe(data => {
+          this.items = data;
+          console.log(this.items);
+
+          catchError(ErrorUtils.handleError<any>('getVideo')));      
+    }else{
+      return this.getVideos();
+    } 
+    //return this.getOTP(id).subscribe();    
   }
 
-  // getVideos(): Observable<any[]> {
-  //   return this.client.get(this.URL_GET_VIDEOS)
-  //     .pipe(
-  //       tap(v => console.log(`videos recuperados w/ id=${v}`)),
-  //         catchError(ErrorUtils.handleError<any>('getVideos')));
-  // }
+  getVideos(): Observable<any[]> {
+    return this.client.get(this.URL_GET_VIDEOS)
+      .pipe(
+        tap(v => console.log(`videos recuperados: ${v}`)),
+          catchError(ErrorUtils.handleError<any>('getVideos')));
+  }
 
   private newPlayer(o,id){
     return new VdoPlayer({
--- src/app/video-aula/video-aula.component.ts
+++ src/app/video-aula/video-aula.component.ts
@@ -6,6 +6,8 @@ import { Aluno } from '../aluno-profile/aluno';
 import { DomSanitizer } from '@angular/platform-browser';
 import { SvpService } from './svp.service';
 import { VdocypherService } from './vdocypher.service';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 @Component({
   selector: 'app-video-aula',
@@ -27,19 +29,17 @@ export class VideoAulaComponent implements OnInit {
     private alunoService:AlunoProfileService,
     private sanitizer: DomSanitizer){}
 
-  ngOnInit() {    
-    // let videoInXML = this.svpService.getVideos();
-    
-    // if(videoInXML){
-    //   let parser = new DOMParser();
-    //   let xmlDoc = parser.parseFromString(videoInXML.toString(),"text/xml");
-    //   let video_list_tag = xmlDoc.childNodes[0].childNodes[3].textContent;
-    //   //let auth_token_tag = xmlDoc.childNodes[0].childNodes[3].textContent;
-    //   console.log(`videos: ${video_list_tag}`); 
-    // }
+  ngOnInit() {        
+    this.vdoService.getVideo('eea6c28b15564829983ee5f2936e09e7')
+      .subscribe(v => { 
+          console.log(v);
+           v.map(data => data.json())
+            .do(data => console.log("User data" + JSON.stringify(data)))
+            .catch(this.handleError);                      
+//        new CognitioMedia(1,)
+      });
+
       
-    this.player = this.vdoService.getVideo('eea6c28b15564829983ee5f2936e09e7');
-    console.log(this.player);
 
     //this.loadVideoAulasByAluno();  
   }
@@ -55,5 +55,31 @@ export class VideoAulaComponent implements OnInit {
   setSafeUrl(v) {
     v.urlSanitized = this.sanitizer.bypassSecurityTrustResourceUrl(v.url);
   }
+
+  private log(msg:string){
+    console.log('PlanoEstudosService: '+msg);
+  }
+
+  public handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+    // TODO: better job of transforming error for user consumption
+    console.log(`${operation} failed: ${error.message}`);
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+    };
+  }
+
+  // parse response da chamada ao SVPService
+  // let videoInXML = this.svpService.getVideos();
+    
+  // if(videoInXML){
+  //   let parser = new DOMParser();
+  //   let xmlDoc = parser.parseFromString(videoInXML.toString(),"text/xml");
+  //   let video_list_tag = xmlDoc.childNodes[0].childNodes[3].textContent;
+  //   //let auth_token_tag = xmlDoc.childNodes[0].childNodes[3].textContent;
+  //   console.log(`videos: ${video_list_tag}`); 
+  // }      
 
 }
